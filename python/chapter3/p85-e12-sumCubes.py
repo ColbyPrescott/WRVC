73a2d4c61a5351aa7c7cb1711f220d7d68e17643
ColbyPrescott <colbyrprescott@gmail.com>
2025-09-16 17:20:00 -0400

    Create p85-e12-sumCubes.py

---
+++ python/chapter3/p85-e12-sumCubes.py
@@ -0,0 +1,14 @@
+# Calcualte the sum of the cubes of the first n natural numbers
+
+def main():
+    print("Calculate the sum of the cubes of the first n natural numbers")
+    n = int(input("Enter number of natural numbers: "))
+    
+    total = 0
+    # Zero is not a natural number but it is still fine to loop through because adding zero^3 does nothing
+    for i in range(n + 1):
+        total += i ** 3
+    
+    print("Sum:", total)
+
+main()
