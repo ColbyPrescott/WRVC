990aad2351da70eae82e7f919453a139ca4878c0
ColbyPrescott <colbyrprescott@gmail.com>
2026-01-06 12:27:34 -0500

    VamSur: Add Player class

--- python/pygame/vampireSurvivor/code/main.py
+++ python/pygame/vampireSurvivor/code/main.py
@@ -1,17 +1,25 @@
 from settings import *
+from player import Player
 
 class Game:
     def __init__(self):
+        # Setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Vampire Survivor")
         self.clock = pygame.time.Clock()
         self.running = True
 
+        # Groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # Sprites
+        self.player = Player((400, 300), self.all_sprites)
+
     def run(self):
         while self.running:
             # Delta time
-            dt = self.clock.tick()
+            dt = self.clock.tick() / 1000
 
             # Event loop
             for event in pygame.event.get():
@@ -19,8 +27,11 @@ class Game:
                     self.running = False
             
             # Update
+            self.all_sprites.update(dt)
 
             # Draw
+            self.display_surface.fill("black")
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
         pygame.quit()
