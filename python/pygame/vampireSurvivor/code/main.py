a292de782f98eff88a5c39bbb9b589931152aa90
ColbyPrescott <colbyrprescott@gmail.com>
2026-01-06 11:28:34 -0500

    VamSur: Add game loop

--- python/pygame/vampireSurvivor/code/main.py
+++ python/pygame/vampireSurvivor/code/main.py
@@ -1,1 +1,30 @@
 from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Vampire Survivor")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            # Delta time
+            dt = self.clock.tick()
+
+            # Event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            # Update
+
+            # Draw
+            pygame.display.update()
+
+        pygame.quit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
