5ed33c0c013a5d40802fd73602d0a8e1cdb98299
ColbyPrescott <colbyrprescott@gmail.com>
2026-01-13 11:59:49 -0500

    VamSur: Add camera

--- python/pygame/vampireSurvivor/code/main.py
+++ python/pygame/vampireSurvivor/code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 from random import randint
 
@@ -15,7 +16,7 @@ class Game:
         self.running = True
 
         # Groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
@@ -47,7 +48,7 @@ class Game:
 
             # Draw
             self.display_surface.fill("black")
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
         pygame.quit()
