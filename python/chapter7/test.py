fa5acccece6626620d52aaebf2d03ab6ac400ded
ColbyPrescott <colbyrprescott@gmail.com>
2026-01-28 12:51:53 -0500

    Update chapter 7

--- python/chapter7/test.py
+++ python/chapter7/test.py
@@ -5,6 +5,6 @@ if True:
                 if not not not not not not not not not not not not not not not not not not not not not not not not True:
                     if not False and not not True or False and not True:
                         if False and not not False or False and (True and not not not False or False) and False or (not not not False):
-                            if True + True == True + True + (not not False) - False - True:
+                            if True + True == True + True + (not not not False) - False - True:
                                 if True + True + True / True - True == - True + (not (True and True or False)) / (True + (not False or False and True)) - (False * False) - True + (True + True) ** (True + (not True) - False + (not False)):
                                     print(":D")
